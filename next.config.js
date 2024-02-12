d6f468d8ba9d8ee607a62dbc0d2bb6b8c553e26c
Magzhan_Zhumabaev <123943870+MagzhanUnited@users.noreply.github.com>
2024-02-12 16:22:57 +0600

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/missdana',
+  basePath: 'https://kazdevo.github.io/missdana',
 }
 
 module.exports = nextConfig
